95829532516b90243a96dd2559520f00ffdbbe3c
TheNesdark <TheNezdark@gmail.com>
2026-01-11 12:51:45 -0500

    fix(orthanc): Actualizar la variable de entorno para la configuración de la API de Orthanc
    - Cambiar la variable de entorno de API_BASE_URL a ORTHANC_URL para mayor claridad.
    - Actualizar el mensaje de error para reflejar el nuevo nombre de la variable y mejorar la gestión de errores.

--- src/config/orthanc.ts
+++ src/config/orthanc.ts
@@ -1,11 +1,11 @@
 import { base64Encode } from "@/utils/StudyUtils";
 // Validar que las variables de entorno estén definidas
-const ORTHANC_URL_ENV = import.meta.env.API_BASE_URL;
+const ORTHANC_URL_ENV = import.meta.env.ORTHANC_URL;
 const ORTHANC_USERNAME_ENV = import.meta.env.ORTHANC_USERNAME;
 const ORTHANC_PASSWORD_ENV = import.meta.env.ORTHANC_PASSWORD;
 
 if (!ORTHANC_URL_ENV) {
-  throw new Error('API_BASE_URL no está definida en las variables de entorno');
+  throw new Error('ORTHANC_URL no está definida en las variables de entorno');
 }
 if (!ORTHANC_USERNAME_ENV) {
   throw new Error('ORTHANC_USERNAME no está definida en las variables de entorno');
